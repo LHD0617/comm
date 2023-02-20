92ec06288fa8b10daef0cbf1e5259416debd4ced
LHD0617 <320388825@qq.com>
2023-02-20 09:37:35 +0800

    202302200937创建点对点串行通信协议

---
+++ comm.h
@@ -0,0 +1,32 @@
+/**
+ * @file comm.h
+ * @author 满心欢喜
+ * @brief 串行通信协议
+ * @version 0.1
+ * @date 2023-02-20
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
+
+#ifndef __COMM_H_
+#define __COMM_H_
+
+/* @include */
+#include <stdlib.h>
+
+/* @typedef */
+typedef unsigned char       comm_uint8;
+typedef unsigned short int  comm_uint16;
+typedef unsigned long int   comm_uint32;
+typedef unsigned long long  comm_uint64;
+
+typedef char                comm_int8;
+typedef short int           comm_int16;
+typedef long  int           comm_int32;
+typedef long  long          comm_int64;
+
+/* @Function declarations */
+
+
+#endif
